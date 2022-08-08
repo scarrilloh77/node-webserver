dbaf185d47a0affa42e5abf4425a6b04f0309c60
Sebastian Carrillo <scarrilloh77@gmail.com>
2022-08-08 01:32:23 -0500

    request and response

--- app.js
+++ app.js
@@ -2,7 +2,26 @@ const http = require("http");
 
 http
   .createServer((req, res) => {
-    res.write("Hola mundo");
+    // res.writeHead(200, { "Content-Type": "text/plain" });
+    // res.writeHead(200, { "Content-Type": "application/json" });
+
+    // const persona = {
+    //   id: 1,
+    //   nombre: "Sebastian",
+    // };
+
+    // res.write(JSON.stringify(persona));
+
+    // res.setHeader("Content-Disposition", "attachment; filename=lista.csv");
+    // res.writeHead(200, { "Content-Type": "application/csv" });
+
+    // res.write("id, nombre\n");
+    // res.write("1, Sebastian\n");
+    // res.write("2, Maria\n");
+    // res.write("3, Juan\n");
+    // res.write("4, Pedro\n");
+
+    res.write("Hola mundo!");
     res.end();
   })
   .listen(8080);
